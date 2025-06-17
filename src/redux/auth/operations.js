2a42f292aee4b3bee3c0557a714b77b14b09d621
Yurii Pasternak <yuriipasternak@icloud.com>
2025-06-17 23:10:55 +0200

    react-08

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5173/";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
 export const register = createAsyncThunk(
   "auth/register",
