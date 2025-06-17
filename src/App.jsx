621c51a857e07665ab3e56ef418aa69c686e46c7
Yurii Pasternak <yuriipasternak@icloud.com>
2025-06-17 23:02:01 +0200

    goit-hw-react-08

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,62 @@
 import { Suspense, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./components/navigation";
+import { useDispatch, useSelector } from "react-redux";
+
+import { refreshUser } from "./redux/auth/operations";
+
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import RegistrationPage from "./pages/registrationPage";
 import ContactsPage from "./pages/contactsPage";
-import AppBar from "./components/appBar";
-import { refreshUser } from "./redux/auth/operations";
-import { useDispatch } from "react-redux";
+
 import Layout from "./components/Layout";
+import PrivateRoute from "./components/PrivateRoute";
+import RestrictedRoute from "./components/RestrictedRoute";
+
 import styles from "./components/componentsCss/app.module.css";
 
 function App() {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector((state) => state.auth.isRefreshing);
+
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
+  if (isRefreshing) {
+    return <p className={styles.loading}>Loading user data...</p>; // або spinner
+  }
+
   return (
     <div className={styles.mainContainer}>
       <Layout>
-        <Suspense fallback={null}>
+        <Suspense fallback={<p>Loading...</p>}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/registration" element={<RegistrationPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute>
+                  <LoginPage />
+                </RestrictedRoute>
+              }
+            />
+            <Route
+              path="/registration"
+              element={
+                <RestrictedRoute>
+                  <RegistrationPage />
+                </RestrictedRoute>
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute>
+                  <ContactsPage />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </Suspense>
       </Layout>
