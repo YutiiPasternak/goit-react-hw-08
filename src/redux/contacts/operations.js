40af910d0b0819697233ce8ff5f127f324bf48e6
Yurii Pasternak <yuriipasternak@icloud.com>
2025-06-17 23:22:45 +0200

    goit-react-08

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://goit-react-hw-08-nu-two.vercel.app/";
+axios.defaults.baseURL = "https://connections-api.goit.global/";
 
 const getAuthHeader = (token) => ({
   headers: {
